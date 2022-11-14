8b8a37fc450c903f19afc4f5b79b294969839a11
Oliver Kokolee <ollie12245@gmail.com>
2022-11-14 10:57:17 -0500

    CMSC Assignment

--- src/components/Handlers/database.js
+++ src/components/Handlers/database.js
@@ -5,6 +5,7 @@ import { openDatabase } from "react-native-sqlite-storage";
 const myContactsDB = openDatabase({name: 'MyContacts.db'});
 const contactsTableName = 'contacts';
 const groupsTableName = 'groups';
+const contactGroupsTableName = 'contact_groups';
 
 module.exports = {
     // declare function that will create the contacts table
@@ -95,4 +96,48 @@ module.exports = {
             );
         });
     },
+
+// declare function that will create the actor films table
+createContactGroupsTable: async function () {
+    // declare a transaction that will execute a SQL statement
+    (await myContactsDB).transaction(txn => {
+        // execute the SQL
+        txn.executeSql(
+            `CREATE TABLE IF NOT EXISTS ${contactGroupsTableName}(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                contact_id INTEGER,
+                group_id INTEGER
+            );`,
+            // arguments needed when using an SQL prepared statement
+            [],
+            // callback function to handle results of SQL query
+            () => {
+                console.log('Contact groups table created successfully');
+            },
+            error => {
+                console.log('Error creating contact groups table ' + error.message);
+            },
+        );
+    });
+},
+
+// declare function that will insert a row into the actor films table
+addContactGroups: async function (contact_id, group_id) {
+    // declare a transaction that will execute an SQL statement
+    (await myContactsDB).transaction(txn => {
+        // execute the SQL
+        txn.executeSql(
+            `INSERT INTO ${contactGroupsTableName} (contact_id, group_id) VALUES (${contact_id}, ${group_id})`,
+            // arguments passed when using SQL prepared statements
+            [],
+            // callback function to handle results of SQL query
+            () => {
+                console.log("Contact group added successfully");
+            },
+            error => {
+                console.log('Error adding contact group ' + error.message);
+            },
+        );
+    });
+},
 };
